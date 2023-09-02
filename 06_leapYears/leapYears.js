d206e7079d450bfede895be65ffa4549c6bc04dd
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-02 16:01:09 -0700

    Update code to determine if a specified year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+/* Leap years have to be divisible by 4 and not by 100 unless they're
+   divisible by 400
+*/
+const leapYears = function(year) {
+    return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)
 };
 
 // Do not edit below this line
