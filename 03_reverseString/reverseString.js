b2bb2365f5416060ddc62389b879f4b3773806d6
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-02 13:46:28 -0700

    Reverse string input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let message = '';
+    for(let i = string.length - 1; i >= 0; i--) {
+        message += string.charAt(i);
+    }
+    return message;
 
+    /* simpler solution
+        return string.split("").reverse().join("");
+    */
 };
 
 // Do not edit below this line
