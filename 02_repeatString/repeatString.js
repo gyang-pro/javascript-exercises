1c7f4a063a053d87584a5fbb4f2294b71db35d7e
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-02 13:48:27 -0700

    Repeat string input a specified number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
+    let message = '';
 
+    if(num < 0) {
+        return 'ERROR';
+    }
+    
+    while(i < num){
+        message += string;
+        i++;
+    }
+    return message;
 };
 
 // Do not edit below this line
