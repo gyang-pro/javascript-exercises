ff73b12ba2ea495b9811e57728d96af476365dfa
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-02 15:25:37 -0700

    Update to remove values if they are included in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array,...remove) {
+    let newArray = []
+    for(let i = 0; i < array.length; i++) {
+        if(!remove.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
 
+    return newArray;
 };
+
+/* more advanced way
+const removeFromArray = function(array, ...args) {
+   return array.filter(val => !args.includes(val))
+ }
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
