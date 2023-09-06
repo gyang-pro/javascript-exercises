2e65e12add675dcb051fd095b6486a2e4aeae746
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-06 15:08:05 -0700

    Check if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = /[^a-z0-9]/g; //any single character BUT a-z and 0-9
+    str = str.toLowerCase().replace(punctuation, '');
+    let str2 = str.split('').reverse().join('');
+    return str === str2;
 };
 
 // Do not edit below this line
