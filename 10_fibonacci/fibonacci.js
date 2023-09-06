0dc741e2f50c1b7b41fb9ff4a0db0010694cfe16
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-06 15:41:08 -0700

    Implement empty function to work as described

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceNum) {
+    if(sequenceNum < 0) {
+        return "OOPS";
+    }
 
+    let first = 1;
+    let second = 1;
+    let current = 1;
+    for(let i = 2; i < sequenceNum; i++) {
+        first = second;
+        second = current;
+        current = first + second;
+    }
+    return current;
 };
 
 // Do not edit below this line
