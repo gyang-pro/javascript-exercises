f79ff2cf0f56337a3e4c87d80c6061c590c3ce9b
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-06 16:08:33 -0700

    Implement empty function to return oldest person in a given array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let newArray = array.map(person => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = (new Date()).getFullYear();
+        }
+        return person;
+    });
 
+    newArray.sort((a,b) => {
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath-b.yearOfBirth;
+        return ageB - ageA;
+    });
+    return newArray[0];
 };
 
 // Do not edit below this line
